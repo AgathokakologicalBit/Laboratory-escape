ba8fd5fa6ad9645e73c38ae5e97a50b4220dee3e
Michael Ermishin <tw.ermishin.michael@gmail.com>
2018-01-18 14:54:20 +0300

    Created a simple moving sprite

--- Laboratory escape/game.cpp
+++ Laboratory escape/game.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "engine/engine.h"
 
+#include "game/test_scene.h"
 
 int main()
 {
 	auto error = Engine::Get().Start();
 	if (error) return error;
+
+	Engine::Get().scene_manager.Load(new TestScene, SceneManager::StatePushMode::APPEND);
 
 	while (Engine::Get().Update()) ;
 	Engine::Get().Stop();
---
+++ Laboratory escape/game/test_scene.cpp
@@ -0,0 +1,19 @@
+#include "../engine/scene.h"
+#include "../engine/engine.h"
+#include "test_scene.h"
+
+void TestScene::Start()
+{
+	sf::Texture * texture = new sf::Texture();
+	texture->loadFromFile("player.png");
+	sprite = sf::Sprite(*texture);
+	sprite.setScale(4, 4);
+}
+
+void TestScene::Update()
+{
+	auto delta = Engine::Get().time.Delta();
+
+	sprite.setPosition(sprite.getPosition() + sf::Vector2f(delta * 50, delta * 25));
+	Engine::Get().window.draw(sprite);
+}
